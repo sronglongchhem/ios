e7783b78c7943d74c57b3fe2fb62bb73a01984b5
Desislav Hristov <hristov.desislav@gmail.com>
2020-05-18 12:18:25 +0300

    ✅

--- Modules/Timer/TimerTests/Reducers/StartEdit/StartEditReducerTests+Autocomplete.swift
+++ Modules/Timer/TimerTests/Reducers/StartEdit/StartEditReducerTests+Autocomplete.swift
@@ -6,6 +6,8 @@ import OtherServices
 
 extension StartEditReducerTests {
     
+    // MARK: Time Entries
+    
     func test_autocomplete_withNoTokens_returnsTimeEntriesMatchingByDescription() {
         let timeEntries = ["Match", "Match as well", "Not this", "Nor this"].enumerated()
             .map { TimeEntry.with(id: Int64($0.offset), description: $0.element) }
@@ -215,6 +217,112 @@ extension StartEditReducerTests {
             reducer: reducer,
             steps:
             descriptionEnteredStep(for: "word1 word2"),
+            Step(.receive, StartEditAction.autocompleteSuggestionsUpdated(expectedSuggestions)) {
+                $0.autocompleteSuggestions = expectedSuggestions
+            }
+        )
+    }
+    
+    // MARK: Projects
+    
+    func test_autocomplete_withProjectToken_returnsProjectsMatchingByName() {
+        let projects = ["Match", "Match as well", "Not this", "Nor this"].enumerated()
+            .map { Project.with(id: Int64($0.offset), name: $0.element) }
+        
+        var expectedSuggestions = projects[..<2].sorted(by: {leftHand, rightHand in
+            leftHand.name > rightHand.name
+        }).map(AutocompleteSuggestion.projectSuggestion)
+        expectedSuggestions.insert(AutocompleteSuggestion.createProjectSuggestion(name: "Match"), at: 0)
+        
+        var entities = TimeLogEntities()
+        entities.projects = Dictionary(uniqueKeysWithValues: projects.map { ($0.id, $0) })
+        
+        let editableTimeEntry = EditableTimeEntry.empty(workspaceId: mockUser.defaultWorkspace)
+        let state = StartEditState(
+            user: Loadable.loaded(mockUser),
+            entities: entities,
+            editableTimeEntry: editableTimeEntry,
+            autocompleteSuggestions: [],
+            dateTimePickMode: .none)
+
+        assertReducerFlow(
+            initialState: state,
+            reducer: reducer,
+            steps:
+            descriptionEnteredStep(for: "@Match"),
+            Step(.receive, StartEditAction.autocompleteSuggestionsUpdated(expectedSuggestions)) {
+                $0.autocompleteSuggestions = expectedSuggestions
+            }
+        )
+    }
+    
+    func test_autocomplete_withProjectToken_returnsProjectsMatchingByClientName() {
+        let clients = ["Match", "Match as well", "Not this", "Nor this"].enumerated()
+            .map { Client(id: Int64($0.offset), name: $0.element, workspaceId: mockUser.defaultWorkspace)}
+        let projects = clients.map { client -> Project in
+            Project.with(name: "Project with client \(client.id)", clientId: client.id)
+        }
+        
+        var expectedSuggestions = projects[..<2].sorted(by: {leftHand, rightHand in
+            leftHand.name > rightHand.name
+        }).map(AutocompleteSuggestion.projectSuggestion)
+        expectedSuggestions.insert(AutocompleteSuggestion.createProjectSuggestion(name: "Match"), at: 0)
+        
+        var entities = TimeLogEntities()
+        entities.clients = Dictionary(uniqueKeysWithValues: clients.map { ($0.id, $0) })
+        entities.projects = Dictionary(uniqueKeysWithValues: projects.map { ($0.id, $0) })
+        
+        let editableTimeEntry = EditableTimeEntry.empty(workspaceId: mockUser.defaultWorkspace)
+        let state = StartEditState(
+            user: Loadable.loaded(mockUser),
+            entities: entities,
+            editableTimeEntry: editableTimeEntry,
+            autocompleteSuggestions: [],
+            dateTimePickMode: .none)
+
+        assertReducerFlow(
+            initialState: state,
+            reducer: reducer,
+            steps:
+            descriptionEnteredStep(for: "@Match"),
+            Step(.receive, StartEditAction.autocompleteSuggestionsUpdated(expectedSuggestions)) {
+                $0.autocompleteSuggestions = expectedSuggestions
+            }
+        )
+    }
+    
+    func test_autocomplete_withProjectToken_returnsProjectsMatchingMultipleWordsAcrossMultipleEntities() {
+        let tags = tagsForAutocompletionTest()
+        let clients = clientsForAutocompletionTest()
+        let projects = projectsForAutocompletionTest()
+        let tasks = tasksForAutocompletionTest()
+        let timeEntries = timeEntriesForAutocompletionTest()
+        
+        var expectedSuggestions = [projects.first!, projects.last!].sorted(by: {leftHand, rightHand in
+                leftHand.name > rightHand.name
+            }).map(AutocompleteSuggestion.projectSuggestion)
+        expectedSuggestions.insert(AutocompleteSuggestion.createProjectSuggestion(name: "word1 word2"), at: 0)
+        
+        var entities = TimeLogEntities()
+        entities.tags = Dictionary(uniqueKeysWithValues: tags.map { ($0.id, $0) })
+        entities.clients = Dictionary(uniqueKeysWithValues: clients.map { ($0.id, $0) })
+        entities.projects = Dictionary(uniqueKeysWithValues: projects.map { ($0.id, $0) })
+        entities.tasks = Dictionary(uniqueKeysWithValues: tasks.map { ($0.id, $0) })
+        entities.timeEntries = Dictionary(uniqueKeysWithValues: timeEntries.map { ($0.id, $0) })
+        
+        let editableTimeEntry = EditableTimeEntry.empty(workspaceId: mockUser.defaultWorkspace)
+        let state = StartEditState(
+            user: Loadable.loaded(mockUser),
+            entities: entities,
+            editableTimeEntry: editableTimeEntry,
+            autocompleteSuggestions: [],
+            dateTimePickMode: .none)
+
+        assertReducerFlow(
+            initialState: state,
+            reducer: reducer,
+            steps:
+            descriptionEnteredStep(for: "Testing @word1 word2"),
             Step(.receive, StartEditAction.autocompleteSuggestionsUpdated(expectedSuggestions)) {
                 $0.autocompleteSuggestions = expectedSuggestions
             }
